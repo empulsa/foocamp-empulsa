580d5e15ad27fdda150cad8decccbbcb401b6013
Leonardo Perez <leperez@google.com>
2021-09-05 22:52:52 -0400

    Add feature that builds all the pages dynamically sent from contentful using the slug as a dynamic paramenter. Add divider component to implement curves that make transition from one section to another more organic.

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,15 @@
-import getPageData from '../utils/api';
-import RelevantInfoCards from '../components/RelevantInfoCards';
-import ComponentNotFound from '../components/ComponentNotFound';
+import Index from './[slug]';
+import { getPageData } from '../utils/api';
 
-export const getServerSideProps = async () => {
-  const pageData = await getPageData('/');
-
+export const getStaticProps = async () => {
+  const slug = 'home';
+  const pageData = await getPageData(slug);
   return {
     props: {
-      data: pageData,
       components: pageData.fields.components,
+      slug,
     },
   };
 };
 
-const AvailableComponents = {
-  // mainNavbar: Navigation,
-  relevantInfoCards: RelevantInfoCards,
-};
-
-export default function Recipes({ components }) {
-  const getComponentType = (component) => {
-    const componentType = component.sys.contentType.sys.id || 'componentNotFound';
-    return componentType;
-  };
-
-  const getComponentId = (component) => component.sys.id || Math.floor(Math.random() * 100);
-
-  const getComponentTemplate = (component, componentType) => {
-    const TargetComponent = AvailableComponents[componentType];
-    return TargetComponent ? <TargetComponent {...component} />
-      : <ComponentNotFound />;
-  };
-
-  const getSectionStyles = ({ fields }) => {
-    const styles = {};
-    if (Object.prototype.hasOwnProperty.call(fields, 'sectionBackgroundColor')) {
-      styles['--sectionBackgroundColor'] = fields.sectionBackgroundColor;
-    }
-    if (Object.prototype.hasOwnProperty.call(fields, 'textColor')) {
-      styles['--textColor'] = fields.textColor;
-    }
-    return styles;
-  };
-
-  return (
-    <main className="home">
-      {components.map((component) => {
-        const componentType = getComponentType(component);
-        return (
-          <section
-            className={componentType}
-            key={getComponentId(component)}
-            style={getSectionStyles(component)}
-          >
-            <div className="section-content">
-              {getComponentTemplate(component, componentType)}
-            </div>
-          </section>
-        );
-      })}
-    </main>
-  );
-}
+export default Index;
