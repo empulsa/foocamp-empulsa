e94d671e915015e1ff807711553dc9dfcece1b9f
Juan Larrotta <jlarrotta@merqueo.com>
2021-09-16 17:06:39 -0500

    ajustes code review

--- components/linksWithIconSection.js
+++ components/linksWithIconSection.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 export default function linksWithIconSection({ fields }) {
   const {
@@ -7,16 +8,26 @@ export default function linksWithIconSection({ fields }) {
     textColor,
     links,
   } = fields;
-  // const { title, text, links, textColor} = fields;
+  const getSectionHeadlines = (colorText) => {
+    const getSectionTitle = Object.prototype.hasOwnProperty.call(fields, 'title') ? <h2 className="linksWithIconSection__title" style={colorText}>{title}</h2> : false;
+    const getSectionText = Object.prototype.hasOwnProperty.call(fields, 'text') ? <p className="linksWithIconSection__text" style={colorText}>{documentToReactComponents(text)}</p> : false;
+
+    return (
+      <>
+        {getSectionTitle}
+        {getSectionText}
+      </>
+    );
+  };
 
   const colorText = { color: textColor };
   const renderLinks = (link) => {
     const { linkUrl, linkLabel, linkIcon } = link.fields;
     return (
-      <li className="boost__item" key={link.sys.id}>
+      <li className="linksWithIconSection__item" key={link.sys.id}>
         <Link href={linkUrl}>
-          <a href={linkUrl} className="boost__item__link">
-            <div className="boost__item__img">
+          <a href={linkUrl} className="linksWithIconSection__item__link">
+            <div className="linksWithIconSection__item__img">
               <img
                 src={linkIcon.fields.file.url}
                 alt={linkIcon.fields.title}
@@ -24,7 +35,7 @@ export default function linksWithIconSection({ fields }) {
                 height={linkIcon.fields.file.details.image.height}
               />
             </div>
-            <p className="boost__item__text" style={colorText}>
+            <p className="linksWithIconSection__item__text" style={colorText}>
               {linkLabel}
             </p>
           </a>
@@ -34,14 +45,11 @@ export default function linksWithIconSection({ fields }) {
   };
 
   return (
-    <section className="boost">
-      <h3 className="boost__title" style={colorText}>
-        {title}
-      </h3>
-      <p className="boost__text" style={colorText}>
-        {text.content[0].content[0].value}
-      </p>
-      <ul className="boost__list">{links.map((link) => renderLinks(link))}</ul>
-    </section>
+    <>
+      {getSectionHeadlines(colorText)}
+      <div className="linksWithIconSection__content">
+        <ul className="linksWithIconSection__list">{links.map((link) => renderLinks(link))}</ul>
+      </div>
+    </>
   );
 }
