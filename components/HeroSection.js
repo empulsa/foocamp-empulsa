1ba0d2f8d9d0313483e4ed684b589016570167a7
Adrian Priale <sinuhe-10@hotmail.com>
2021-10-01 11:08:56 -0500

    Test deleted

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,13 +2,11 @@ import HeroItem from './HeroItem';
 
 export default function HeroSection({ fields }) {
   const { heroItems } = fields;
-  const heroValue = heroItems[0].fields;
-  const heroValue2 = heroItems[1].fields;
-  heroValue2.textOnLeft = false;
   return (
     <>
-      <HeroItem fields={heroValue} />
-      <HeroItem fields={heroValue2} />
+      {heroItems.map((heroItem) => (
+        <HeroItem fields={heroItem.fields} key={heroItem.sys.id} />
+      ))}
     </>
   );
 }
