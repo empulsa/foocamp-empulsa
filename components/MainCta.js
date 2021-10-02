1b0ff90afa73b002f01ae39c9f744698f612c0c6
Julieth Salazar <Juli.Salazar18@hotmail.com>
2021-10-02 16:18:38 -0500

    Heart icon of the main cta added

--- components/MainCta.js
+++ components/MainCta.js
@@ -2,8 +2,11 @@ import Link from 'next/link';
 
 export default function MainCta({ fields }) {
   return (
-    <Link href="{fields.linkUrl}">
-      <button className="main-cta" type="button">{fields.linkLabel}</button>
+    <Link href={fields.linkUrl}>
+      <button className="main-cta" type="button">
+        <img className="main-cta__icon" src={fields.icon.fields.file.url} width="20" height="18" alt="Heart icon" />
+        {fields.linkLabel}
+      </button>
     </Link>
   );
 }
