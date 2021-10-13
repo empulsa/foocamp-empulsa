17f680c12cbd7ddf5494b2f04b91e55770d3094e
Adrian Priale <sinuhe-10@hotmail.com>
2021-10-13 14:44:27 -0500

    Hamburger component and navbar at 99%

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import MainCta from './MainCta';
+import Hamburger from './Hamburger';
 
 export default function NavegationMain({ fields }) {
   const [menu, setMenu] = useState(false);
+  const { logo, navItems, ctaButton } = fields;
 
   return (
     <>
@@ -11,61 +13,50 @@ export default function NavegationMain({ fields }) {
         ? 'mainNavbar__wrapper mainNavbar__wrapper--mobile-active'
         : 'mainNavbar__wrapper'}
       >
-        <button className="mainNavbar__burguer" type="button" onClick={() => { setMenu(!menu); }}>
-          ☰
-        </button>
-        <Link href="/">
-          <img className="mainNavbar__img" src={fields.logo.fields.image.fields.file.url} alt={fields.logo.fields.nombre} />
-        </Link>
+        <Hamburger
+          active={menu}
+          onClick={() => { setMenu(!menu); }}
+        />
+        <div className="mainNavbar__brand">
+          <Link href="/">
+            <img className="mainNavbar__img" src={logo.fields.image.fields.file.url} alt={fields.logo.fields.nombre} />
+          </Link>
+        </div>
         <ul className="mainNavbar__ul">
-          <li className="mainNavbar__li">
-            <a href={`${fields.navItems[0].fields.linkUrl}`} className="mainNavbar__link">
-              {fields.navItems[0].fields.linkLabel}
-            </a>
-          </li>
-          <li className="mainNavbar__li ">
-            <a className="mainNavbar__link arrow" href={`${fields.navItems[1].fields.linkUrl}`}>
-              {fields.navItems[1].fields.linkLabel}
-            </a>
-            <ul className="mainNavbar__subUl">
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[1].fields.subLinks[0].fields.linkUrl}`}>{fields.navItems[1].fields.subLinks[0].fields.linkLabel}</a>
+          {navItems.map((navItem) => {
+            const { linkUrl, linkLabel, subLinks } = navItem.fields;
+            const arrowClass = subLinks ? 'arrow' : '';
+            return (
+              <li className="mainNavbar__li" key={navItem.sys.id}>
+                <Link href={`${linkUrl}`}>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a className={`mainNavbar__link ${arrowClass}`}>
+                    {linkLabel}
+                  </a>
+                </Link>
+                {subLinks
+                && (
+                <ul className="mainNavbar__subUl">
+                  {subLinks.map((subLink) => (
+                    <li className="mainNavbar__subLi" key={subLink.sys.id}>
+                      <Link href={`${subLink.fields.linkUrl}`}>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a className="mainNavbar__link">
+                          {subLink.fields.linkLabel}
+                        </a>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+                )}
               </li>
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[1].fields.subLinks[1].fields.linkUrl}`}>{fields.navItems[1].fields.subLinks[1].fields.linkLabel}</a>
-              </li>
-            </ul>
-          </li>
-          <li className="mainNavbar__li">
-            <a className="mainNavbar__link arrow" href={`${fields.navItems[2].fields.linkUrl}`}>
-              {fields.navItems[2].fields.linkLabel}
-            </a>
-            <ul className="mainNavbar__subUl">
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[2].fields.subLinks[0].fields.linkUrl}`}>{fields.navItems[2].fields.subLinks[0].fields.linkLabel}</a>
-              </li>
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[2].fields.subLinks[1].fields.linkUrl}`}>{fields.navItems[2].fields.subLinks[1].fields.linkLabel}</a>
-              </li>
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[2].fields.subLinks[2].fields.linkUrl}`}>{fields.navItems[2].fields.subLinks[2].fields.linkLabel}</a>
-              </li>
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[2].fields.subLinks[3].fields.linkUrl}`}>{fields.navItems[2].fields.subLinks[3].fields.linkLabel}</a>
-              </li>
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[2].fields.subLinks[4].fields.linkUrl}`}>{fields.navItems[2].fields.subLinks[4].fields.linkLabel}</a>
-              </li>
-              <li className="mainNavbar__subLi">
-                <a className="mainNavbar__link" href={`${fields.navItems[2].fields.subLinks[5].fields.linkUrl}`}>{fields.navItems[2].fields.subLinks[5].fields.linkLabel}</a>
-              </li>
-            </ul>
-          </li>
+            );
+          })}
           <li>
             <MainCta fields={fields.ctaButton.fields} className="mainNavbar__btn" />
           </li>
         </ul>
-        <MainCta fields={fields.ctaButton.fields} className="mainNavbar__btn mainNavbar__heart" />
+        <MainCta fields={ctaButton.fields} className="mainNavbar__btn mainNavbar__heart" />
       </div>
     </>
   );
